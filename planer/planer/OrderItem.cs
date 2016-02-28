61354e682898144a3c51ac7bd76a08d6c1c6e0da
nasiek <n.paruzel@gmail.com>
2016-02-28 18:14:30 +0100

    order + orderItem

---
+++ planer/planer/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace planer
+{
+    public class OrderItem
+    {
+        public int Amount { get; set; }
+        public int Width { get; set; }
+        public int Length { get; set; }
+
+    }
+}
