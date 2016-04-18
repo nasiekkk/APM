a1e88296e9b4a2eb978d5de08fa380fbd81613e6
nasiek <n.paruzel@gmail.com>
2016-04-18 22:41:37 +0200

    tables - mvvm

--- planer/planer/Order.cs
+++ planer/planer/Order.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Planer.MVVM_Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,14 @@ using System.Threading.Tasks;
 
 namespace Planer
 {
-    public class Order
+    public class Order : ObservableObject
     {
         public DateTime OrderDate { get; set; }
-        public List<OrderItem> Items { get; set; }
+        public List<OrderItem> Items { get; set; } 
+        
+        
     }
+
+   
+
 }
