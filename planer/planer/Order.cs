ee76aaec1a4ebad899b27ae68311236fad13fb51
nasiek <n.paruzel@gmail.com>
2016-06-06 19:44:14 +0200

    ModifyOrderCommand and CalculateOrderCommand added, c'tors Order and OrderItem added

--- planer/planer/Order.cs
+++ planer/planer/Order.cs
@@ -10,9 +10,18 @@ namespace Planer
     public class Order : ObservableObject
     {
         public DateTime OrderDate { get; set; }
-        public List<OrderItem> Items { get; set; } 
-        
-        
+        public List<OrderItem> Items { get; set; }
+
+        public Order() { }
+        public Order(int n)
+        {
+            if (Items == null)
+            {
+                Items = new List<OrderItem>();
+            }
+            Items.Add(new OrderItem(n));
+
+        }
     }
 
    
--- planer/planer/OrderItem.cs
+++ planer/planer/OrderItem.cs
@@ -12,5 +12,18 @@ namespace Planer
         public int Width { get; set; }
         public int Length { get; set; }
 
+        public OrderItem()
+        {
+            Amount = 0;
+            Width = 0;
+            Length = 0;
+        }
+
+        public OrderItem(int amount)
+        {
+            Amount = amount;
+            Width = 0;
+            Length = 0;
+        }
     }
 }
