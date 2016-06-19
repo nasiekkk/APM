d00443348b3df64cfdc91fd42618b6b966dbd453
nasiek <n.paruzel@gmail.com>
2016-06-19 20:11:04 +0200

    added buttons

---
+++ planer/planer/MVVM_Core/ObservableObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace Planer.MVVM_Core
+{
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            var handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        protected void RaisePropertyChanged(String propertyName)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ planer/planer/MainWindowVM.cs
@@ -0,0 +1,91 @@
+﻿using Planer.MVVM_Core;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Planer
+{
+    public class MainWindowVM : ObservableObject
+    {
+        private ObservableCollection<Order> _orders;
+        public ObservableCollection<Order> Orders
+        {
+            get 
+            { 
+                return _orders; 
+            }
+            set
+            {
+                _orders = value;
+                RaisePropertyChanged("Orders");
+            }
+         
+        }
+        
+        public ICommand AddOrderCommand { get; set; }
+        public ICommand ModifyOrderCommand { get; set; }
+        public ICommand CalculateOrderCommand { get; set; }
+        private Order _selectedOrder;
+        public Order SelectedOrder
+        {
+            get
+            {
+                return _selectedOrder;
+            }
+            set
+            {
+                _selectedOrder = value;
+                RaisePropertyChanged("SelectedOrder");
+            }
+        }
+
+        public MainWindowVM()
+        {
+            Orders = new ObservableCollection<Order>
+            {
+                new Order
+                {
+                    OrderDate = DateTime.Now,
+                    Items = new List<OrderItem> 
+                    { 
+                        new OrderItem(), 
+                        new OrderItem()
+                    }
+                },
+                new Order
+                {
+                    OrderDate = DateTime.Today,
+                    Items = new List<OrderItem> 
+                    { 
+                        new OrderItem(), 
+                        new OrderItem()
+                    }
+                }
+            };
+            AddOrderCommand = new RelayCommand(
+                () => 
+                { 
+                    Orders.Add(new Order());
+                }
+            );
+
+            ModifyOrderCommand = new RelayCommand(
+                () => 
+                {
+                    Orders.Remove(SelectedOrder);
+                    Orders.Add(new Order(4));
+                }
+            );
+
+            CalculateOrderCommand = new RelayCommand(
+                () =>
+                {
+
+                });
+        }
+    }
+}
